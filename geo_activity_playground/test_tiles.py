c00a118d4e6498fbb8430a8de80e8f5b83dc6434
Martin Ueding <mu@martin-ueding.de>
2022-06-09 15:53:59 +0200

    Add conversion back

--- geo_activity_playground/test_tiles.py
+++ geo_activity_playground/test_tiles.py
@@ -1,6 +1,14 @@
-from .tiles import compute_tile
+from .tiles import compute_tile, get_tile_upper_left_lat_lon
 
 
 def test_rheinbach() -> None:
     lat, lon = 50.6202, 6.9504
     assert compute_tile(lat, lon) == (8508, 5512)
+
+
+def test_back() -> None:
+    tile_x, tile_y = 8508, 5512
+    zoom = 14
+    lat, lon = get_tile_upper_left_lat_lon(tile_x, tile_y, zoom)
+    print(lat, lon)
+    assert compute_tile(lat, lon, zoom) == (tile_x, tile_y)
--- geo_activity_playground/tiles.py
+++ geo_activity_playground/tiles.py
@@ -1,3 +1,4 @@
+import math
 from typing import Tuple
 
 import numpy as np
@@ -10,3 +11,11 @@ def compute_tile(lat: float, lon: float, zoom: int = 14) -> Tuple[int, int]:
     y = (1 - y/np.pi)/2
     n = 2**zoom
     return int(x * n), int(y * n)
+
+
+def get_tile_upper_left_lat_lon(tile_x: int, tile_y: int, zoom: int) -> Tuple[float, float]:
+  n = 2.0 ** zoom
+  lon_deg = tile_x / n * 360.0 - 180.0
+  lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * tile_y / n)))
+  lat_deg = math.degrees(lat_rad)
+  return lat_deg, lon_deg
