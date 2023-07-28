48c212e7c392c8a93b81a9a1c3a5712a2ee9ec85
Martin Ueding <mu@martin-ueding.de>
2023-07-28 11:55:53 +0200

    Export to Parquet

--- geo_activity_playground/strava/api_access.py
+++ geo_activity_playground/strava/api_access.py
@@ -3,8 +3,12 @@ import dataclasses
 import datetime
 import logging
 import pickle
+from typing import Any
+from typing import Iterator
 
+import pandas as pd
 from stravalib import Client
+from stravalib.model import Activity
 from tqdm import tqdm
 
 from geo_activity_playground.core.directories import cache_dir
@@ -55,24 +59,6 @@ def get_current_access_token() -> str:
     return tokens["access"]
 
 
-"""
-id
-commute
-distance
-end_latlng
-gear_id
-name
-sport_type
-start_date
-start_latlng
-type
-upload_id
-calories
-description
-gear
-"""
-
-
 def main() -> None:
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
@@ -82,6 +68,9 @@ def main() -> None:
 
     download = subparsers.add_parser("download")
     download.set_defaults(func=main_download)
+
+    parquet = subparsers.add_parser("parquet")
+    parquet.set_defaults(func=main_parquet)
 
     options = parser.parse_args()
     options.func(options)
@@ -107,10 +96,59 @@ def download_activities_after(after: str) -> None:
 
 def main_download(options: argparse.Namespace) -> None:
     last_activity_path = max(activity_cache_dir.glob("*.pickle"))
-    print(last_activity_path)
     with open(last_activity_path, "rb") as f:
         activity = pickle.load(f)
     download_activities_after(activity.start_date.isoformat().replace("+00:00", "Z"))
+
+
+def iter_all_activities() -> Iterator[Activity]:
+    for path in activity_cache_dir.glob("*.pickle"):
+        with open(path, "rb") as f:
+            yield pickle.load(f)
+
+
+"""
+id
+commute
+distance
+end_latlng
+gear_id
+name
+sport_type
+start_date
+start_latlng
+type
+upload_id
+calories
+description
+gear
+"""
+
+
+def make_activity_dict(activity: Activity) -> dict[str, Any]:
+    result = {
+        "id": activity.id,
+        "commute": activity.commute,
+        "distance": activity.distance.magnitude,
+        "name": activity.name,
+        "type": activity.type,
+        "start_date": activity.start_date,
+    }
+    if activity.start_latlng is not None and activity.end_latlng is not None:
+        result.update(
+            {
+                "start_lat": activity.start_latlng.lat,
+                "start_lon": activity.start_latlng.lon,
+                "end_lat": activity.end_latlng.lat,
+                "end_lon": activity.end_latlng.lon,
+            }
+        )
+    return result
+
+
+def main_parquet(options: argparse.Namespace) -> None:
+    df = pd.DataFrame(map(make_activity_dict, iter_all_activities()))
+    df.to_parquet("activities.parquet")
 
 
 if __name__ == "__main__":
