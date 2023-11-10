d5a3c86942f9944336b506912ef8aa6f798ab457
Martin Ueding <mu@martin-ueding.de>
2023-11-10 08:02:49 +0100

    Stable hash from path

--- geo_activity_playground/importers/directory.py
+++ geo_activity_playground/importers/directory.py
@@ -1,3 +1,4 @@
+import hashlib
 import logging
 import pathlib
 
@@ -22,7 +23,7 @@ def import_from_directory() -> None:
     activity_stream_dir = pathlib.Path("Cache/Activity Timeseries")
     new_rows: list[dict] = []
     for path in pathlib.Path("Activities").rglob("*.*"):
-        id = hash(path)
+        id = int(hashlib.md5(str(path).encode()).hexdigest(), 16) % 2**62
         if id in already_parsed:
             continue
 
