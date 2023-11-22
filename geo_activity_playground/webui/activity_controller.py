1ba287448ea508bac10bfb01bc037f97e4db59a7
Martin Ueding <mu@martin-ueding.de>
2023-11-22 21:30:49 +0100

    Add more plots to each activity

--- geo_activity_playground/webui/activity_controller.py
+++ geo_activity_playground/webui/activity_controller.py
@@ -2,6 +2,7 @@ import functools
 
 import altair as alt
 import geojson
+import pandas as pd
 
 from geo_activity_playground.core.activities import ActivityRepository
 from geo_activity_playground.core.activities import make_geojson_from_time_series
@@ -18,7 +19,49 @@ class ActivityController:
         time_series = self._repository.get_time_series(id)
         line_json = make_geojson_from_time_series(time_series)
 
-        return {
+        result = {
             "activity": activity,
             "line_json": line_json,
+            "distance_time_plot": distance_time_plot(time_series),
         }
+        if "altitude" in time_series.columns:
+            result["altitude_time_plot"] = altitude_time_plot(time_series)
+        if "heartrate" in time_series.columns:
+            result["heartrate_time_plot"] = heartrate_time_plot(time_series)
+        return result
+
+
+def distance_time_plot(time_series: pd.DataFrame) -> str:
+    return (
+        alt.Chart(time_series, title="Distance")
+        .mark_line()
+        .encode(alt.X("time", title="Time"), alt.Y("distance", title="Distance / km"))
+        .interactive()
+        .to_json(format="vega")
+    )
+
+
+def altitude_time_plot(time_series: pd.DataFrame) -> str:
+    return (
+        alt.Chart(time_series, title="Altitude")
+        .mark_line()
+        .encode(
+            alt.X("time", title="Time"),
+            alt.Y("altitude", scale=alt.Scale(zero=False), title="Altitude / m"),
+        )
+        .interactive()
+        .to_json(format="vega")
+    )
+
+
+def heartrate_time_plot(time_series: pd.DataFrame) -> str:
+    return (
+        alt.Chart(time_series, title="Heart Rate")
+        .mark_line()
+        .encode(
+            alt.X("time", title="Time"),
+            alt.Y("heartrate", scale=alt.Scale(zero=False), title="Heart rate"),
+        )
+        .interactive()
+        .to_json(format="vega")
+    )
