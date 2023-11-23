ce31957266a1e2c792954f9e2356efbff59f2445
Martin Ueding <mu@martin-ueding.de>
2023-11-23 20:53:57 +0100

    Use work tracker also for reading activities

--- geo_activity_playground/importers/directory.py
+++ geo_activity_playground/importers/directory.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from geo_activity_playground.core.activity_parsers import read_activity
 from geo_activity_playground.core.coordinates import get_distance
+from geo_activity_playground.core.tasks import work_tracker
 
 logger = logging.getLogger(__name__)
 
@@ -19,49 +20,49 @@ def import_from_directory() -> None:
         logger.info("Didn't find a metadata file.")
         meta = None
 
-    already_parsed = set(meta.id) if meta is not None else set()
+    with work_tracker(pathlib.Path("Cache/parsed_activities.json")) as already_parsed:
+        activity_stream_dir = pathlib.Path("Cache/Activity Timeseries")
+        activity_stream_dir.mkdir(exist_ok=True, parents=True)
+        new_rows: list[dict] = []
+        for path in pathlib.Path("Activities").rglob("*.*"):
+            id = int(hashlib.sha3_224(str(path).encode()).hexdigest(), 16) % 2**62
+            if id in already_parsed:
+                continue
+            else:
+                already_parsed.add(id)
 
-    activity_stream_dir = pathlib.Path("Cache/Activity Timeseries")
-    activity_stream_dir.mkdir(exist_ok=True, parents=True)
-    new_rows: list[dict] = []
-    for path in pathlib.Path("Activities").rglob("*.*"):
-        id = int(hashlib.sha3_224(str(path).encode()).hexdigest(), 16) % 2**62
-        if id in already_parsed:
-            continue
+            logger.info(f"Parsing activity file {path} …")
+            timeseries = read_activity(path)
+            if len(timeseries) == 0:
+                continue
+            timeseries_path = activity_stream_dir / f"{id}.parquet"
+            timeseries.to_parquet(timeseries_path)
 
-        logger.info(f"Parsing activity file {path} …")
-        timeseries = read_activity(path)
-        timeseries_path = activity_stream_dir / f"{id}.parquet"
-        print(timeseries)
-        timeseries.to_parquet(timeseries_path)
+            distances = [
+                get_distance(lat_1, lon_1, lat_2, lon_2)
+                for lat_1, lon_1, lat_2, lon_2 in zip(
+                    timeseries["latitude"],
+                    timeseries["longitude"],
+                    timeseries["latitude"].iloc[1:],
+                    timeseries["longitude"].iloc[1:],
+                )
+            ]
+            distance = sum(distances)
 
-        distances = [
-            get_distance(lat_1, lon_1, lat_2, lon_2)
-            for lat_1, lon_1, lat_2, lon_2 in zip(
-                timeseries["latitude"],
-                timeseries["longitude"],
-                timeseries["latitude"].iloc[1:],
-                timeseries["longitude"].iloc[1:],
+            new_rows.append(
+                {
+                    "id": id,
+                    "commute": None,
+                    "distance": distance,
+                    "name": path.stem,
+                    "kind": None,
+                    "start": timeseries["time"].iloc[0],
+                    "elapsed_time": timeseries["time"].iloc[-1]
+                    - timeseries["time"].iloc[0],
+                    "equipment": None,
+                    "calories": 0,
+                }
             )
-        ]
-
-        print(len(distances))
-        distance = sum(distances)
-
-        new_rows.append(
-            {
-                "id": id,
-                "commute": None,
-                "distance": distance,
-                "name": path.stem,
-                "kind": None,
-                "start": timeseries["time"].iloc[0],
-                "elapsed_time": timeseries["time"].iloc[-1]
-                - timeseries["time"].iloc[0],
-                "equipment": None,
-                "calories": 0,
-            }
-        )
 
     new_df = pd.DataFrame(new_rows)
     merged: pd.DataFrame = pd.concat([meta, new_df])
