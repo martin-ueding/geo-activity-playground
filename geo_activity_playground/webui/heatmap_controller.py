57de0aa4d7772c7d68784332533e7be976bee30c
Martin Ueding <mu@martin-ueding.de>
2023-11-24 13:52:18 +0100

    Filter the relevant points

--- geo_activity_playground/webui/heatmap_controller.py
+++ geo_activity_playground/webui/heatmap_controller.py
@@ -1,14 +1,22 @@
 import functools
 import io
+import logging
+
+import pandas as pd
 
 from geo_activity_playground.core.activities import ActivityRepository
 from geo_activity_playground.core.heatmap import get_all_points
 from geo_activity_playground.core.tiles import get_tile
+from geo_activity_playground.core.tiles import get_tile_upper_left_lat_lon
+
+
+logger = logging.getLogger(__name__)
 
 
 class HeatmapController:
     def __init__(self, repository: ActivityRepository) -> None:
         self._repository = repository
+        self._all_points = pd.DataFrame()
 
     @functools.cache
     def render(self) -> dict:
@@ -23,7 +31,18 @@ class HeatmapController:
         }
 
     def render_tile(self, x: int, y: int, z: int) -> bytes:
-        tile = get_tile(z, x, y)
+        lat_min, lon_min = get_tile_upper_left_lat_lon(x, y, z)
+        lat_max, lon_max = get_tile_upper_left_lat_lon(x + 1, y + 1, z)
+
+        logger.info(f"Filtering relevant points for {x}/{y} at {z} …")
+        relevant_points = self._all_points.loc[
+            (lat_min <= self._all_points["latitude"])
+            & (self._all_points["latitude"] <= lat_max)
+            & (lon_min <= self._all_points["longitude"])
+            & (self._all_points["longitude"] <= lon_max)
+        ]
+
+        map_tile = get_tile(z, x, y)
         f = io.BytesIO()
-        tile.save(f, format="png")
+        map_tile.save(f, format="png")
         return bytes(f.getbuffer())
