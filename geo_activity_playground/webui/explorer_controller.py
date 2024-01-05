1cd10daca0e82ffbf43bbb1966cd0e67c673e240
Martin Ueding <mu@martin-ueding.de>
2024-01-05 19:54:54 +0100

    Move symbols in file

--- geo_activity_playground/webui/explorer_controller.py
+++ geo_activity_playground/webui/explorer_controller.py
@@ -29,6 +29,81 @@ from geo_activity_playground.explorer.tile_visits import TileEvolutionState
 alt.data_transformers.enable("vegafusion")
 
 
+class ExplorerController:
+    def __init__(self, repository: ActivityRepository) -> None:
+        self._repository = repository
+
+    @functools.cache
+    def render(self, zoom: int) -> dict:
+        with open(TILE_EVOLUTION_STATES_PATH, "rb") as f:
+            tile_evolution_states = pickle.load(f)
+        with open(TILE_VISITS_PATH, "rb") as f:
+            tile_visits = pickle.load(f)
+        with open(TILE_HISTORIES_PATH, "rb") as f:
+            tile_histories = pickle.load(f)
+
+        medians = tile_histories[zoom].median()
+        median_lat, median_lon = get_tile_upper_left_lat_lon(
+            medians["tile_x"], medians["tile_y"], zoom
+        )
+
+        explored = get_three_color_tiles(
+            tile_visits[zoom], self._repository, tile_evolution_states[zoom], zoom
+        )
+
+        return {
+            "center": {
+                "latitude": median_lat,
+                "longitude": median_lon,
+                "bbox": bounding_box_for_biggest_cluster(
+                    tile_evolution_states[zoom].clusters.values(), zoom
+                )
+                if len(tile_evolution_states[zoom].memberships) > 0
+                else {},
+            },
+            "explored": explored,
+            "plot_tile_evolution": plot_tile_evolution(tile_histories[zoom]),
+            "plot_cluster_evolution": plot_cluster_evolution(
+                tile_evolution_states[zoom].cluster_evolution
+            ),
+            "plot_square_evolution": plot_square_evolution(
+                tile_evolution_states[zoom].square_evolution
+            ),
+            "zoom": zoom,
+        }
+
+    def export_missing_tiles(self, zoom, north, east, south, west, suffix: str) -> str:
+        x1, y1 = compute_tile(north, west, zoom)
+        x2, y2 = compute_tile(south, east, zoom)
+        tile_bounds = Bounds(x1, y1, x2 + 2, y2 + 2)
+
+        with open(TILE_HISTORIES_PATH, "rb") as f:
+            tile_histories = pickle.load(f)
+        tiles = tile_histories[zoom]
+        points = get_border_tiles(tiles, zoom, tile_bounds)
+        if suffix == "geojson":
+            return make_grid_file_geojson(points)
+        elif suffix == "gpx":
+            return make_grid_file_gpx(points)
+
+    def export_explored_tiles(self, zoom, north, east, south, west, suffix: str) -> str:
+        x1, y1 = compute_tile(north, west, zoom)
+        x2, y2 = compute_tile(south, east, zoom)
+        tile_bounds = Bounds(x1, y1, x2 + 2, y2 + 2)
+
+        with open(TILE_VISITS_PATH, "rb") as f:
+            tile_visits = pickle.load(f)
+        tiles = tile_visits[zoom]
+        points = make_grid_points(
+            (tile for tile in tiles.keys() if tile_bounds.contains(*tile)), zoom
+        )
+        if suffix == "geojson":
+            return make_grid_file_geojson(points)
+        elif suffix == "gpx":
+            return make_grid_file_gpx(points)
+        ...
+
+
 def get_three_color_tiles(
     tile_visits: dict,
     repository: ActivityRepository,
@@ -170,81 +245,6 @@ def bounding_box_for_biggest_cluster(
             ),
         )
     )
-
-
-class ExplorerController:
-    def __init__(self, repository: ActivityRepository) -> None:
-        self._repository = repository
-
-    @functools.cache
-    def render(self, zoom: int) -> dict:
-        with open(TILE_EVOLUTION_STATES_PATH, "rb") as f:
-            tile_evolution_states = pickle.load(f)
-        with open(TILE_VISITS_PATH, "rb") as f:
-            tile_visits = pickle.load(f)
-        with open(TILE_HISTORIES_PATH, "rb") as f:
-            tile_histories = pickle.load(f)
-
-        medians = tile_histories[zoom].median()
-        median_lat, median_lon = get_tile_upper_left_lat_lon(
-            medians["tile_x"], medians["tile_y"], zoom
-        )
-
-        explored = get_three_color_tiles(
-            tile_visits[zoom], self._repository, tile_evolution_states[zoom], zoom
-        )
-
-        return {
-            "center": {
-                "latitude": median_lat,
-                "longitude": median_lon,
-                "bbox": bounding_box_for_biggest_cluster(
-                    tile_evolution_states[zoom].clusters.values(), zoom
-                )
-                if len(tile_evolution_states[zoom].memberships) > 0
-                else {},
-            },
-            "explored": explored,
-            "plot_tile_evolution": plot_tile_evolution(tile_histories[zoom]),
-            "plot_cluster_evolution": plot_cluster_evolution(
-                tile_evolution_states[zoom].cluster_evolution
-            ),
-            "plot_square_evolution": plot_square_evolution(
-                tile_evolution_states[zoom].square_evolution
-            ),
-            "zoom": zoom,
-        }
-
-    def export_missing_tiles(self, zoom, north, east, south, west, suffix: str) -> str:
-        x1, y1 = compute_tile(north, west, zoom)
-        x2, y2 = compute_tile(south, east, zoom)
-        tile_bounds = Bounds(x1, y1, x2 + 2, y2 + 2)
-
-        with open(TILE_HISTORIES_PATH, "rb") as f:
-            tile_histories = pickle.load(f)
-        tiles = tile_histories[zoom]
-        points = get_border_tiles(tiles, zoom, tile_bounds)
-        if suffix == "geojson":
-            return make_grid_file_geojson(points)
-        elif suffix == "gpx":
-            return make_grid_file_gpx(points)
-
-    def export_explored_tiles(self, zoom, north, east, south, west, suffix: str) -> str:
-        x1, y1 = compute_tile(north, west, zoom)
-        x2, y2 = compute_tile(south, east, zoom)
-        tile_bounds = Bounds(x1, y1, x2 + 2, y2 + 2)
-
-        with open(TILE_VISITS_PATH, "rb") as f:
-            tile_visits = pickle.load(f)
-        tiles = tile_visits[zoom]
-        points = make_grid_points(
-            (tile for tile in tiles.keys() if tile_bounds.contains(*tile)), zoom
-        )
-        if suffix == "geojson":
-            return make_grid_file_geojson(points)
-        elif suffix == "gpx":
-            return make_grid_file_gpx(points)
-        ...
 
 
 def plot_tile_evolution(tiles: pd.DataFrame) -> str:
