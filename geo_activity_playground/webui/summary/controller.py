0880f6699eb327f8ecd048a278fa63d625a05314
Martin Ueding <mu@martin-ueding.de>
2024-06-28 18:12:21 +0200

    GH-115: Make summary statistics page nicer

--- geo_activity_playground/webui/summary/controller.py
+++ geo_activity_playground/webui/summary/controller.py
@@ -1,3 +1,4 @@
+import datetime
 import functools
 
 import altair as alt
@@ -12,19 +13,54 @@ class SummaryController:
 
     @functools.cache
     def render(self) -> dict:
+        df = embellished_activities(self._repository.meta)
+
+        year_kind_total = (
+            df[["year", "kind", "distance_km", "hours"]]
+            .groupby(["year", "kind"])
+            .sum()
+            .reset_index()
+        )
+
         return {
-            "distance_heatmap_meta_plot": distance_heatmap_meta_plot(
-                self._repository.meta
-            ),
-            "year_on_year_distance_meta_plot": year_on_year_distance_meta_plot(
-                self._repository.meta
-            ),
+            "plot_distance_heatmap": plot_distance_heatmap(df),
+            "plot_monthly_distance": plot_monthly_distance(df),
+            "plot_yearly_distance": plot_yearly_distance(year_kind_total),
+            "plot_year_cumulative": plot_year_cumulative(df),
+            "tabulate_year_kind_mean": tabulate_year_kind_mean(df)
+            .reset_index()
+            .to_dict(orient="split"),
+            "plot_weekly_distance": plot_weekly_distance(df),
         }
 
 
-def distance_heatmap_meta_plot(meta: pd.DataFrame) -> str:
+def embellished_activities(meta: pd.DataFrame) -> pd.DataFrame:
+    df = meta.copy()
+    df["year"] = [start.year for start in df["start"]]
+    df["month"] = [start.month for start in df["start"]]
+    df["day"] = [start.day for start in df["start"]]
+    df["week"] = [start.isocalendar().week for start in df["start"]]
+    df["hours"] = [
+        elapsed_time.total_seconds() / 3600 for elapsed_time in df["elapsed_time"]
+    ]
+    del df["elapsed_time"]
+    return df
+
+
+def plot_distance_heatmap(meta: pd.DataFrame) -> str:
     return (
-        alt.Chart(meta)
+        alt.Chart(
+            meta.loc[
+                (
+                    meta["start"]
+                    >= pd.to_datetime(
+                        datetime.datetime.now(datetime.UTC)
+                        - datetime.timedelta(days=2 * 365)
+                    )
+                )
+            ],
+            title="Daily Distance Heatmap",
+        )
         .mark_rect()
         .encode(
             alt.X("date(start):O", title="Day of month"),
@@ -46,15 +82,117 @@ def distance_heatmap_meta_plot(meta: pd.DataFrame) -> str:
     )
 
 
-def year_on_year_distance_meta_plot(meta: pd.DataFrame) -> str:
+def plot_monthly_distance(meta: pd.DataFrame) -> str:
     return (
-        alt.Chart(meta, title="Year on Year Distance")
+        alt.Chart(
+            meta.loc[
+                (
+                    meta["start"]
+                    >= pd.to_datetime(
+                        datetime.datetime.now(datetime.UTC)
+                        - datetime.timedelta(days=2 * 365)
+                    )
+                )
+            ],
+            title="Montly Distance",
+        )
         .mark_bar()
         .encode(
-            alt.X("month(start)"),
-            alt.Y("sum(distance_km)"),
+            alt.X("month(start)", title="Month"),
+            alt.Y("sum(distance_km)", title="Distance / km"),
             alt.Color("kind", scale=alt.Scale(scheme="category10"), title="Kind"),
+            alt.Column("year(start):O", title="Year"),
         )
-        .facet(facet="year(start):O", columns=4)
+        .resolve_axis(x="independent")
+        .to_json(format="vega")
+    )
+
+
+def plot_yearly_distance(year_kind_total: pd.DataFrame) -> str:
+    return (
+        alt.Chart(year_kind_total)
+        .mark_bar()
+        .encode(
+            alt.X("year:O", title="Year"),
+            alt.Y("distance_km", title="Distance / km"),
+            alt.Color("kind", title="Kind"),
+        )
+        .to_json(format="vega")
+    )
+
+
+def plot_year_cumulative(df: pd.DataFrame) -> str:
+    year_cumulative = (
+        df[["year", "week", "distance_km"]]
+        .groupby("year")
+        .apply(
+            lambda group: pd.DataFrame(
+                {"week": group["week"], "distance_km": group["distance_km"].cumsum()}
+            ),
+            include_groups=False,
+        )
+        .reset_index()
+    )
+
+    return (
+        alt.Chart(year_cumulative, title="Cumultative Distance per Year")
+        .mark_line()
+        .encode(
+            alt.X("week", title="Week"),
+            alt.Y("distance_km", title="Distance / km"),
+            alt.Color("year:N", title="Year"),
+            alt.Tooltip("year"),
+        )
+        .to_json(format="vega")
+    )
+
+
+def tabulate_year_kind_mean(df: pd.DataFrame) -> pd.DataFrame:
+    year_kind_mean = (
+        df[["year", "kind", "distance_km", "hours"]]
+        .groupby(["year", "kind"])
+        .mean()
+        .reset_index()
+    )
+
+    year_kind_mean_distance = year_kind_mean.pivot(
+        index="year", columns="kind", values="distance_km"
+    )
+
+    return year_kind_mean_distance
+
+
+def plot_weekly_distance(df: pd.DataFrame) -> str:
+    week_kind_total_distance = (
+        df[["year", "week", "kind", "distance_km"]]
+        .groupby(["year", "week", "kind"])
+        .sum()
+        .reset_index()
+    )
+    week_kind_total_distance["year_week"] = [
+        f"{year}-{week:02d}"
+        for year, week in zip(
+            week_kind_total_distance["year"], week_kind_total_distance["week"]
+        )
+    ]
+
+    last_year = week_kind_total_distance["year"].iloc[-1]
+    last_week = week_kind_total_distance["week"].iloc[-1]
+
+    return (
+        alt.Chart(
+            week_kind_total_distance.loc[
+                (week_kind_total_distance["year"] == last_year)
+                | (week_kind_total_distance["year"] == last_year - 1)
+                & (week_kind_total_distance["week"] >= last_week)
+            ],
+            title="Weekly Distance",
+        )
+        .mark_bar()
+        .encode(
+            alt.X("year_week", title="Year and Week"),
+            alt.Y("distance_km", title="Distance / km"),
+            alt.Color("kind", title="Kind"),
+        )
         .to_json(format="vega")
     )
