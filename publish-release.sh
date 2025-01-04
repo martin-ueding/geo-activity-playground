43a61eb82e3bcece2e83ae634eca1118890c1b86
Martin Ueding <mu@martin-ueding.de>
2025-01-04 12:35:51 +0100

    Embellish release script

--- publish-release.sh
+++ publish-release.sh
@@ -13,3 +13,5 @@ poetry build
 poetry publish --username __token__ --password "$PYPI_TOKEN_GEO_ACTIVITY_PLAYGROUND"
 
 poetry run mkdocs gh-deploy
+
+echo 'Go to https://github.com/martin-ueding/geo-activity-playground/releases/new to create a new release on GitHub.'
