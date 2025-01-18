19d391c46dcb2998d0a9b17c7e34ae09a9a97fae
Martin Ueding <mu@martin-ueding.de>
2025-01-18 18:22:24 +0100

    Write a test for equipment filter

--- geo_activity_playground/core/test_meta_search.py
+++ geo_activity_playground/core/test_meta_search.py
@@ -28,6 +28,25 @@ def test_empty_query() -> None:
     assert (actual["id"] == activity_meta["id"]).all()
 
 
+def test_equipment_query() -> None:
+    activity_meta = pd.DataFrame(
+        {
+            "equipment": pd.Series(["A", "B", "B"]),
+            "id": pd.Series([1, 2, 3]),
+            "kind": pd.Series(["X", "X", "Y"]),
+            "name": ["Test1", "Test2", "Test3"],
+            "start": [
+                datetime.datetime(2024, 12, 24, 10),
+                datetime.datetime(2025, 1, 1, 10),
+                None,
+            ],
+        }
+    )
+    search_query = SearchQuery(equipment=["B"])
+    actual = apply_search_query(activity_meta, search_query)
+    assert set(actual["id"]) == {2, 3}
+
+
 def test_make_mask() -> None:
     assert (_make_mask(2, True) == pd.Series([True, True])).all()
     assert (_make_mask(2, False) == pd.Series([False, False])).all()
