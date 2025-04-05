311e6370abd340f30e3ed97127e3eb2c35edbbe2
Martin Ueding <mu@martin-ueding.de>
2025-04-05 08:39:56 +0200

    Unify tile routes with strategy pattern

--- geo_activity_playground/webui/tile_blueprint.py
+++ geo_activity_playground/webui/tile_blueprint.py
@@ -15,14 +15,14 @@ from geo_activity_playground.core.raster_map import TileGetter
 
 class TileView(View):
     def __init__(
-        self, image_transform: ImageTransform, tile_getter: TileGetter
+        self, image_transforms: dict[str, ImageTransform], tile_getter: TileGetter
     ) -> None:
-        self._image_transform = image_transform
+        self._image_transforms = image_transforms
         self._tile_getter = tile_getter
 
-    def dispatch_request(self, z: int, x: int, y: int) -> Response:
+    def dispatch_request(self, scheme: str, z: int, x: int, y: int) -> Response:
         map_tile = np.array(self._tile_getter.get_tile(z, x, y)) / 255
-        transformed_tile = self._image_transform.transform_image(map_tile)
+        transformed_tile = self._image_transforms[scheme].transform_image(map_tile)
         f = io.BytesIO()
         pl.imsave(f, transformed_tile, format="png")
         return Response(bytes(f.getbuffer()), mimetype="image/png")
@@ -30,17 +30,14 @@ class TileView(View):
 
 def register_tile_routes(app: Flask, tile_getter: TileGetter):
     app.add_url_rule(
-        "/tile/color/<int:z>/<int:x>/<int:y>.png",
-        "tile_color",
-        TileView(IdentityImageTransform(), tile_getter).dispatch_request,
-    )
-    app.add_url_rule(
-        "/tile/grayscale/<int:z>/<int:x>/<int:y>.png",
-        "tile_grayscale",
-        TileView(GrayscaleImageTransform(), tile_getter).dispatch_request,
-    )
-    app.add_url_rule(
-        "/tile/pastel/<int:z>/<int:x>/<int:y>.png",
-        "tile_pastel",
-        TileView(PastelImageTransform(), tile_getter).dispatch_request,
+        "/tile/<scheme>/<int:z>/<int:x>/<int:y>.png",
+        "tile",
+        TileView(
+            {
+                "color": IdentityImageTransform(),
+                "grayscale": GrayscaleImageTransform(),
+                "pastel": PastelImageTransform(),
+            },
+            tile_getter,
+        ).dispatch_request,
     )
