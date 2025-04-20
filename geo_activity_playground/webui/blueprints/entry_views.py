a386c91c0aa3103ded27a080a28981bdffb636a1
Martin Ueding <mu@martin-ueding.de>
2025-04-20 09:34:22 +0200

    GH-263: Fix startup without activities

--- geo_activity_playground/webui/blueprints/entry_views.py
+++ geo_activity_playground/webui/blueprints/entry_views.py
@@ -32,19 +32,19 @@ def register_entry_views(
                 repository.meta, kind_scale, column_elevation_gain
             )
 
-        meta = repository.meta.copy()
-        meta["date"] = meta["start"].dt.date
+            meta = repository.meta
 
-        context["latest_activities"] = collections.defaultdict(list)
-        for date, activity_meta in list(meta.groupby("date"))[:-30:-1]:
-            for index, activity in activity_meta.iterrows():
-                time_series = repository.get_time_series(activity["id"])
-                context["latest_activities"][date].append(
-                    {
-                        "activity": activity,
-                        "line_geojson": make_geojson_from_time_series(time_series),
-                    }
-                )
+            context["latest_activities"] = collections.defaultdict(list)
+            for date, activity_meta in list(meta.groupby("date"))[:-30:-1]:
+                for index, activity in activity_meta.iterrows():
+                    time_series = repository.get_time_series(activity["id"])
+                    context["latest_activities"][date].append(
+                        {
+                            "activity": activity,
+                            "line_geojson": make_geojson_from_time_series(time_series),
+                        }
+                    )
+
         return render_template("home.html.j2", **context)
 
 
