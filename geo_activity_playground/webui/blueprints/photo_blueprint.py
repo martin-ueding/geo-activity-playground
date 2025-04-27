fbfdffd4c98f17fcd47c4558504a2b248bf211d7
Martin Ueding <mu@martin-ueding.de>
2025-04-27 12:12:00 +0200

    feat: Add map with all photos, GH-247

--- geo_activity_playground/webui/blueprints/photo_blueprint.py
+++ geo_activity_playground/webui/blueprints/photo_blueprint.py
@@ -73,6 +73,32 @@ def make_photo_blueprint(
         with open(small_path, "rb") as f:
             return Response(f.read(), mimetype="image/webp")
 
+    @blueprint.route("/map")
+    def map() -> str:
+        return render_template("photo/map.html.j2")
+
+    @blueprint.route("/map-for-all/photos.geojson")
+    def map_for_all() -> Response:
+        photos = DB.session.scalars(sqlalchemy.select(Photo)).all()
+        fc = geojson.FeatureCollection(
+            features=[
+                geojson.Feature(
+                    geometry=geojson.Point((photo.longitude, photo.latitude)),
+                    properties={
+                        "photo_id": photo.id,
+                        "url_marker": url_for(".get", id=photo.id, size=128),
+                        "url_popup": url_for(".get", id=photo.id, size=512),
+                        "url_full": url_for(".get", id=photo.id, size=4096),
+                    },
+                )
+                for photo in photos
+            ]
+        )
+        return Response(
+            geojson.dumps(fc, sort_keys=True, indent=2, ensure_ascii=False),
+            mimetype="application/json",
+        )
+
     @blueprint.route("/map-for-activity/<int:activity_id>/photos.geojson")
     def map_for_activity(activity_id: int) -> Response:
         activity = DB.session.get_one(Activity, activity_id)
