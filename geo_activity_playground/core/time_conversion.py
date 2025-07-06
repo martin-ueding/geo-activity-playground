b19cf518e194f90d035a0f8d609d68a9b75373c8
Martin Ueding <mu@martin-ueding.de>
2025-07-06 12:26:19 +0200

    Restore old version of convert_to_datetime_ns

--- geo_activity_playground/core/time_conversion.py
+++ geo_activity_playground/core/time_conversion.py
@@ -13,9 +13,12 @@ from .paths import USER_CACHE_DIR
 def convert_to_datetime_ns(date) -> np.datetime64 | pd.Series:
     if isinstance(date, pd.Series):
         ts = pd.to_datetime(date)
+        ts = ts.dt.tz_localize(None)
         return ts
     else:
         ts = pd.to_datetime(date)
+        if ts.tzinfo is not None:
+            ts = ts.tz_localize(None)
         return ts.to_datetime64()
 
 
