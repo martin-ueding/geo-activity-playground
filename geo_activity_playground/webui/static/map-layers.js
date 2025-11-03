6a2269e1c054d2775b4dfd9ace0dd71f58191afd
Martin Ueding <mu@martin-ueding.de>
2025-11-03 20:29:13 +0100

    GH-366: Add activity line and tile layers

---
+++ geo_activity_playground/webui/static/map-layers.js
@@ -0,0 +1,60 @@
+function add_layers_to_map(map, zoom, map_tile_attribution, base = 'Grayscale', overlay = "Colorful Cluster") {
+    let base_maps = {
+        "Grayscale": L.tileLayer("/tile/grayscale/{z}/{x}/{y}.png", {
+            maxZoom: 19,
+            attribution: map_tile_attribution
+        }),
+        "Pastel": L.tileLayer("/tile/pastel/{z}/{x}/{y}.png", {
+            maxZoom: 19,
+            attribution: map_tile_attribution
+        }),
+        "Color": L.tileLayer("/tile/color/{z}/{x}/{y}.png", {
+            maxZoom: 19,
+            attribution: map_tile_attribution
+        }),
+        "Inverse Grayscale": L.tileLayer("/tile/inverse_grayscale/{z}/{x}/{y}.png", {
+            maxZoom: 19,
+            attribution: map_tile_attribution
+        }),
+    }
+
+    let overlay_maps = {
+        "Colorful Cluster": L.tileLayer(`/explorer/${zoom}/tile/{z}/{x}/{y}.png?color_strategy=colorful_cluster`, {
+            maxZoom: 19,
+            attribution: map_tile_attribution
+        }),
+        "Max Cluster": L.tileLayer(`/explorer/${zoom}/tile/{z}/{x}/{y}.png?color_strategy=max_cluster`, {
+            maxZoom: 19,
+            attribution: map_tile_attribution
+        }),
+        "First Visit": L.tileLayer(`/explorer/${zoom}/tile/{z}/{x}/{y}.png?color_strategy=first`, {
+            maxZoom: 19,
+            attribution: map_tile_attribution
+        }),
+        "Last Visit": L.tileLayer(`/explorer/${zoom}/tile/{z}/{x}/{y}.png?color_strategy=last`, {
+            maxZoom: 19,
+            attribution: map_tile_attribution
+        }),
+        "Number of Visits": L.tileLayer(`/explorer/${zoom}/tile/{z}/{x}/{y}.png?color_strategy=visits`, {
+            maxZoom: 19,
+            attribution: map_tile_attribution
+        }),
+        "Visited": L.tileLayer(`/explorer/${zoom}/tile/{z}/{x}/{y}.png?color_strategy=visited`, {
+            maxZoom: 19,
+            attribution: map_tile_attribution
+        }),
+        "Missing": L.tileLayer(`/explorer/${zoom}/tile/{z}/{x}/{y}.png?color_strategy=missing`, {
+            maxZoom: 19,
+            attribution: map_tile_attribution
+        }),
+        "Heatmap": L.tileLayer("/heatmap/tile/{z}/{x}/{y}.png", {
+            maxZoom: 19,
+            attribution: map_tile_attribution
+        }),
+    }
+
+    base_maps[base].addTo(map)
+    overlay_maps[overlay].addTo(map)
+
+    var layerControl = L.control.layers(base_maps, overlay_maps).addTo(map);
+}
