014935feb444cfde5f642cb3268fb48c201df27b
Martin Ueding <mu@martin-ueding.de>
2025-11-16 10:49:09 +0100

    Render tooltip on server

--- geo_activity_playground/webui/static/server-side-explorer.js
+++ geo_activity_playground/webui/static/server-side-explorer.js
@@ -20,36 +20,11 @@ if (bbox) {
 
 map.on('click', e => {
     fetch(`/explorer/${zoom}/info/${e.latlng.lat}/${e.latlng.lng}`)
-        .then(response => response.json())
-        .then(data => {
-            if (!data.tile_xy) {
-                return;
-            }
-            console.debug(data);
-
-            let lines = [
-                `<dt>Tile</dt>`,
-                `<dd>${data.tile_xy}</dd>`,
-                `<dt>First visit</dt>`,
-                `<dd>${data.first_time}</br><a href=/activity/${data.first_activity_id}>${data.first_activity_name}</a></dd>`,
-                `<dt>Last visit</dt>`,
-                `<dd>${data.last_time}</br><a href=/activity/${data.last_activity_id}>${data.last_activity_name}</a></dd>`,
-                `<dt>Number of visits</dt>`,
-                `<dd>${data.num_visits}</dd>`,
-            ]
-            if (data.this_cluster_size) {
-                lines.push(`<dt>This cluster size</dt><dd>${data.this_cluster_size}</dd>`)
-                if (data.new_bookmark_url === undefined) {
-                    console.error("Somehow we got a cluster that doesn't have a proper URL. This is the data that we got in response:")
-                    console.error(data)
-                } else {
-                    lines.push(`<dt>Bookmark</dt><dd><a href="${data.new_bookmark_url}">Create cluster bookmark</a></dd>`)
-                }
-            }
-
+        .then(response => response.text())
+        .then(text => {
             L.popup()
                 .setLatLng(e.latlng)
-                .setContent('<dl>' + lines.join('') + '</dl>')
+                .setContent(text)
                 .openOn(map);
         }
         );
