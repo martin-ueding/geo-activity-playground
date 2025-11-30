a7a1d2e6092bc613e8c3a792a5f3e9107a2ca41a
Martin Ueding <mu@martin-ueding.de>
2025-11-30 09:49:36 +0100

    GH-236: Show segments with little maps

--- geo_activity_playground/webui/blueprints/segments_blueprint.py
+++ geo_activity_playground/webui/blueprints/segments_blueprint.py
@@ -1,3 +1,5 @@
+import geojson
+import sqlalchemy
 from flask import Blueprint
 from flask import redirect
 from flask import render_template
@@ -20,9 +22,11 @@ def make_segments_blueprint(
     blueprint = Blueprint("segments", __name__, template_folder="templates")
 
     @blueprint.route("/")
-    @needs_authentication(authenticator)
     def index():
-        return render_template("segments/index.html.j2")
+        return render_template(
+            "segments/index.html.j2",
+            segments=DB.session.scalars(sqlalchemy.select(Segment)).all(),
+        )
 
     @blueprint.route("/new", methods=["POST"])
     @needs_authentication(authenticator)
@@ -46,5 +50,19 @@ def make_segments_blueprint(
 
             flasher.flash_message(f"Created segment “{name}”.", FlashTypes.SUCCESS)
         return redirect(url_for(".index"))
+
+    @blueprint.route("/line/<int:id>/line.geojson")
+    def line(id: int) -> ResponseReturnValue:
+        segment = DB.session.get_one(Segment, id)
+        gj = geojson.Feature(
+            geometry=geojson.LineString(
+                coordinates=[[lon, lat] for lat, lon in segment.coordinates]
+            )
+        )
+        return geojson.dumps(gj)
+
+    @blueprint.route("/show/<int:id>")
+    def show(id: int) -> ResponseReturnValue:
+        pass
 
     return blueprint
