89ac4bc6bc94c524433a2c88eb73758752e2a2ed
Martin Ueding <mu@martin-ueding.de>
2025-11-30 09:08:07 +0100

    GH-236: Get test to pass

--- geo_activity_playground/core/segments.py
+++ geo_activity_playground/core/segments.py
@@ -2,4 +2,6 @@ import geojson
 
 
 def extract_segment_from_geojson(geojson_str: str) -> list[tuple[float, float]]:
-    pass
+    gj = geojson.loads(geojson_str)
+    coordinates = gj["features"][0]["geometry"]["coordinates"]
+    return [(c[0], c[1]) for c in coordinates]
