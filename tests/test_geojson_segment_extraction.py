c703a12dbed10124d3662e39a45f0692ab4c1643
Martin Ueding <mu@martin-ueding.de>
2025-11-30 09:12:20 +0100

    GH-236: Use lat-lon order

--- tests/test_geojson_segment_extraction.py
+++ tests/test_geojson_segment_extraction.py
@@ -6,5 +6,5 @@ from geo_activity_playground.core.segments import extract_segment_from_geojson
 def test_extract_segment_from_geojson() -> None:
     geojson_str = pathlib.Path("tests/kp-niederkassler-straße.geojson").read_text()
     latlon = extract_segment_from_geojson(geojson_str)
-    assert latlon[0] == (7.113486, 50.761744)
-    assert latlon[-1] == (7.112859, 50.759869)
+    assert latlon[0] == (50.761744, 7.113486)
+    assert latlon[-1] == (50.759869, 7.112859)
