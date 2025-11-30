5b842f28a3cd153330bae81d9c65b26c0977648d
Martin Ueding <mu@martin-ueding.de>
2025-11-30 09:12:20 +0100

    GH-236: Use lat-lon order

--- geo_activity_playground/core/segments.py
+++ geo_activity_playground/core/segments.py
@@ -4,4 +4,4 @@ import geojson
 def extract_segment_from_geojson(geojson_str: str) -> list[tuple[float, float]]:
     gj = geojson.loads(geojson_str)
     coordinates = gj["features"][0]["geometry"]["coordinates"]
-    return [(c[0], c[1]) for c in coordinates]
+    return [(c[1], c[0]) for c in coordinates]
