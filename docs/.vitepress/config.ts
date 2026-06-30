9a89afe5c09610611a4ab9969c94d7c8807b5a7a
Martin Ueding <mu@martin-ueding.de>
2026-06-30 20:43:40 +0200

    docs: reorder navigation

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -12,8 +12,14 @@ export default {
     ],
     sidebar: [
       { text: 'Home', link: '/' },
-      { text: 'Getting Started', link: '/getting-started' },
-      { text: 'Screenshot Tour', link: '/screenshot-tour' },
+      {
+        text: 'Overview',
+        items: [
+          { text: 'Getting Started', link: '/getting-started' },
+          { text: 'Screenshot Tour', link: '/screenshot-tour' },
+          { text: 'Similar Projects', link: '/similar-projects' },
+        ],
+      },
       {
         text: 'Installation',
         items: [
@@ -21,69 +27,78 @@ export default {
           { text: 'Install on Windows', link: '/install-on-windows' },
           { text: 'Install on macOS', link: '/install-on-macos' },
           { text: 'Add Local Bin to PATH', link: '/add-local-bin-to-path' },
-        ],
-      },
-      {
-        text: 'Getting Started',
-        items: [
           { text: 'Create a Base Directory', link: '/create-a-base-directory' },
           { text: 'Starting the Program', link: '/starting-the-webserver' },
-          { text: 'Record Activities', link: '/record-activities' },
         ],
       },
       {
-        text: 'Activity Sources',
+        text: 'Docker Installation',
         items: [
-          { text: 'Choosing an Activity Source', link: '/activity-sources' },
-          { text: 'Import Activity Files', link: '/import-activity-files' },
-          { text: 'Connect Strava API', link: '/connect-strava-api' },
-          { text: 'Upload Activity Files', link: '/upload-activity-files' },
-          { text: 'Moving from Strava', link: '/moving-from-strava' },
-        ],
-      },
-      {
-        text: 'How-To Guides',
-        items: [
-          { text: 'Using Maps as Overlays', link: '/using-maps-as-overlays' },
-          { text: 'Build Custom Plots', link: '/build-custom-plots' },
-          { text: 'Plan Missing Tile Rides', link: '/plan-missing-tile-rides' },
-          { text: 'Explorer Tiles on the Go', link: '/explorer-tiles-on-the-go' },
-          { text: 'Cluster History', link: '/cluster-history' },
-          { text: 'Create a Privacy Zone', link: '/create-a-privacy-zone' },
-          { text: 'Set Up a Development Environment', link: '/set-up-a-development-environment' },
-          { text: 'Rename Activity Kinds', link: '/rename-activity-kinds' },
           { text: 'Using Git Version via Docker', link: '/using-git-version-via-docker' },
           { text: 'Using Git Version via Docker Compose', link: '/using-git-version-via-docker-compose' },
           { text: 'Docker Compose + Tailscale VPN', link: '/using-git-version-via-docker-compose-and-tailscale-vpn' },
+        ],
+      },
+      {
+        text: 'Importing Activities',
+        items: [
+          { text: 'Choosing an Activity Source', link: '/activity-sources' },
+          { text: 'Import Activity Files', link: '/import-activity-files' },
+          { text: 'Upload Activity Files', link: '/upload-activity-files' },
+          { text: 'Connect Strava API', link: '/connect-strava-api' },
+          { text: 'Moving from Strava', link: '/moving-from-strava' },
+          { text: 'Record Activities', link: '/record-activities' },
+          { text: 'Rename Activity Kinds', link: '/rename-activity-kinds' },
+          { text: 'Advanced Metadata Extraction', link: '/advanced-metadata-extraction' },
+        ],
+      },
+      {
+        text: 'Explorer Tiles',
+        items: [
+          { text: 'Explorer Tiles', link: '/explorer-tiles' },
+          { text: 'Plan Missing Tile Rides', link: '/plan-missing-tile-rides' },
+          { text: 'Explorer Tiles on the Go', link: '/explorer-tiles-on-the-go' },
+          { text: 'Cluster History', link: '/cluster-history' },
+          { text: 'Incremental Cluster Algorithm', link: '/incremental-cluster-algorithm' },
+        ],
+      },
+      {
+        text: 'Maps & Plots',
+        items: [
+          { text: 'Using Maps as Overlays', link: '/using-maps-as-overlays' },
+          { text: 'Build Custom Plots', link: '/build-custom-plots' },
+          { text: 'Create a Privacy Zone', link: '/create-a-privacy-zone' },
+        ],
+      },
+      {
+        text: 'Metrics',
+        items: [
+          { text: 'Eddington Number', link: '/eddington-number' },
+          { text: 'Heart Rate Zones', link: '/heart-rate-zones' },
+          { text: 'Elevation Gain from Noisy Data', link: '/elevation-gain-from-noisy-data' },
+          { text: "Satellite Elevation Isn't Helpful", link: '/satellite-elevation-isnt-helpful' },
+          { text: 'Segment Matching', link: '/segment-matching' },
+          { text: 'Time Zone Handling Sucks', link: '/time-zone-handling-sucks' },
+        ],
+      },
+      {
+        text: 'Contributing',
+        items: [
+          { text: 'Set Up a Development Environment', link: '/set-up-a-development-environment' },
           { text: 'Change Database Schema', link: '/change-database-schema' },
           { text: 'Update and Extend Translations', link: '/update-and-extend-translations' },
         ],
       },
       {
-        text: 'Reference',
+        text: 'About',
         items: [
-          { text: 'Advanced Metadata Extraction', link: '/advanced-metadata-extraction' },
+          { text: 'Contributors', link: '/contributors' },
+          { text: 'Acknowledgments', link: '/acknowledgments' },
+          { text: 'License', link: '/license' },
+          { text: 'Get Help', link: '/get-help' },
+          { text: 'Changelog', link: '/changelog' },
         ],
       },
-      {
-        text: 'Explanation',
-        items: [
-          { text: 'Explorer Tiles', link: '/explorer-tiles' },
-          { text: 'Incremental Cluster Algorithm', link: '/incremental-cluster-algorithm' },
-          { text: 'Eddington Number', link: '/eddington-number' },
-          { text: 'Heart Rate Zones', link: '/heart-rate-zones' },
-          { text: 'Elevation Gain from Noisy Data', link: '/elevation-gain-from-noisy-data' },
-          { text: 'Time Zone Handling Sucks', link: '/time-zone-handling-sucks' },
-          { text: "Satellite Elevation Isn't Helpful", link: '/satellite-elevation-isnt-helpful' },
-          { text: 'Segment Matching', link: '/segment-matching' },
-        ],
-      },
-      { text: 'Contributors', link: '/contributors' },
-      { text: 'Acknowledgments', link: '/acknowledgments' },
-      { text: 'Similar Projects', link: '/similar-projects' },
-      { text: 'License', link: '/license' },
-      { text: 'Get Help', link: '/get-help' },
-      { text: 'Changelog', link: '/changelog' },
     ],
     socialLinks: [
       { icon: 'github', link: 'https://github.com/martin-ueding/geo-activity-playground' },
